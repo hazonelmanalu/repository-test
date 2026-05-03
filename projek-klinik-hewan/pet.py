ce6da31a5363f87f7597a6414c991d6b44e6a652
ahmadmalikalfatah8-rgb <ahmadmalikalfatah_8@student.uns.ac.id>
2026-05-04 00:07:16 +0700

    Update pet.py

--- projek-klinik-hewan/pet.py
+++ projek-klinik-hewan/pet.py
@@ -1,12 +1,13 @@
 class Pet:
+    pet_database = []
     def __init__(self, pet_ID, name, gender, species, race, age, health_status):
-        pet_ID.self = pet_ID
-        name.self = name
-        gender.self = gender
-        species.self = species
-        race.self = race
-        age.self = age
-        health_status.self = health_status
+        self.pet_ID = pet_ID
+        self.name = name
+        self.gender = gender
+        self.species = species
+        self.race = race
+        self.age = age
+        self.health_status = health_status
 
         self.Owner = None
         self.AssignedDoctor = None
@@ -31,11 +32,28 @@ class Pet:
         print(f"Room    : {self.Room}")
         print("---------------------------")
 
-    def update_data():
-        
+    def tambah_data(self):
+        Pet.pet_database.append(self)
+        print(f"Data hewan '{self.name}' berhasil ditambahkan ke database!")
+
+    def replace_data(cls, target_ID, new_pet_object):
+        for i, pet in enumerate(cls.pet_database):
+            if pet.pet_ID == target_ID:
+                cls.pet_database[i] = new_pet_object
+                print(f"[Sukses] Data hewan dengan ID '{target_ID}' telah diganti dengan '{new_pet_object.name}'.")
+                return
+        print(f"[Gagal] Hewan dengan ID '{target_ID}' tidak ditemukan.")
+
+    def hapus_data(cls.pet_database, target_ID):
+        for pet in cls.pet_database:
+            if pet.pet_ID == target_ID:
+                cls.pet_database.remove(pet)
+                print(f"[Sukses] Data hewan dengan ID '{target_ID}' berhasil dihapus.")
+                return
+        print(f"[Gagal] Hewan dengan ID '{target_ID}' tidak ditemukan.")
 
     def set_doctor(self):
-        self.AssignedDoctor = Doctor
+        self.AssignedDoctor = Doctor.name
 
     def set_room(self):
-        self.Room = Room
+        self.Room = Room.name
